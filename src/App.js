832e14333396cc034e09b4fb7d1050ebaf2e146f
Ilhom Aliyev <aliyevilhom99@gmail.com>
2023-04-19 14:26:21 +0500

    currency done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState('USDT');
+  const [toCurrency, setToCurrency] = useState('DRZ');
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
+
+  const ratesRef = useRef({});
+  
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setToPrice(result.toFixed(3));
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(3));
+    setToPrice(value);
+  };
+
+  useEffect(() => {
+    fetch(
+      'http://api.coinlayer.com/live?access_key=66c45c370a8c8fe4f339811341d0dc28'
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current = json.rates;
+        onChangeToPrice(1);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Не удалось получить информацию!');
+      });
+  }, []);
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block
+        value={fromPrice}
+        onChangeValue={onChangeFromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+      />
+      <Block
+        value={toPrice}
+        onChangeValue={onChangeToPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+      />
     </div>
   );
 }
